497a7834c737249fb70e2499908764f2f50bdb8a
ashay-maheshwari <aaryaashay1848@gmail.com>
2021-09-22 17:52:07 +0530

    add new changes

--- dbinit.py
+++ dbinit.py
@@ -44,11 +44,6 @@ def init_collection(dbclient, databasename, collection_name, data_dict):
         records = parkingcol.insert_one(data_dict)
         return records
 
-#connection_string = "mongodb+srv://parkingadmin:parkingadmin@cluster0.uvrnm.mongodb.net/ParkingManagementSystem?retryWrites=true&w=majority"
-#dbname = "ParkingManagementSystem"
-#collection_name = "parkingslots"
-
-
 #Connect to database
 dbclient = connect_db(connection_string, dbname)
 
