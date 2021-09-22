42e206a8349805783dec19beb04574bbc10f18b9
ashay-maheshwari <aaryaashay1848@gmail.com>
2021-09-22 17:40:39 +0530

    add parking collectin init

---
+++ dbinit.py
@@ -0,0 +1,55 @@
+import pymongo
+from parkingslots import *
+print(parking_slots)
+
+def connect_db(connection_uri, database):
+    """
+    :param connection_uri: srv connection uri of the mongodb database
+    :param database: database name to connect with. If database does not exists, it creates one with the given name
+    :return: database client object
+
+    """
+    try:
+        client = pymongo.MongoClient(connection_uri)
+        database_list = client.list_database_names()
+        if database in database_list:
+            print("Database exists")
+            return client
+        else:
+            db = client[database]
+            print(db)
+            print("Database created")
+            return client
+    except:
+        return "connection failed"
+
+def init_collection(dbclient, databasename, collection_name, data_dict):
+    """
+
+    :param dbclient: client object for database
+    :param databasename: name of the database to be used
+    :param collection_name: name of the collection. If does not exists, will be created
+    :param data_dict: data to be inserted
+    :return: status of the insert operation
+    """
+
+    parkingdb = dbclient[databasename]
+    collection_list = parkingdb.list_collection_names()
+    if collection_name in collection_list:
+        print("Collection already exists")
+        return None
+    else:
+        parkingcol = parkingdb[collection_name]
+        records = parkingcol.insert_one(data_dict)
+        return records
+
+connection_string = "mongodb+srv://parkingadmin:parkingadmin@cluster0.uvrnm.mongodb.net/ParkingManagementSystem?retryWrites=true&w=majority"
+dbname = "ParkingManagementSystem"
+collection_name = "parkingslots"
+
+#Connect to database
+dbclient = connect_db(connection_string, dbname)
+
+#Create collection and insert records
+init_data = init_collection(dbclient, dbname, collection_name, parking_slots)
+print(init_data)
---
+++ parkingslots.py
@@ -0,0 +1,58 @@
+parking_slots = {
+"Level-0": {
+    "Motorcycle spots" : {
+        "row1": ["M1", "M2", "M3", "M4", "M5", "M6"],
+        "row2": ["M7", "M8", "M9", "M10", "M11", "M12"]
+},
+    "compact spots" : {
+        "row1" : ["C1", "C2", "C3", "C4", "C5", "C6"],
+        "row2" : ["C7", "C8", "C9", "C10", "C11", "C12"]
+},
+    "large spots": {
+        "row1" : ["L1", "L2", "L3", "L4", "L5", "L6"],
+        "row2" : ["L7", "L8", "L9", "L10", "L11", "L12"]
+}
+},
+"Level-1" : {
+     "Motorcycle spots" : {
+        "row1": ["M1", "M2", "M3", "M4", "M5", "M6"],
+        "row2": ["M7", "M8", "M9", "M10", "M11", "M12"]
+},
+    "compact spots" : {
+        "row1" : ["C1", "C2", "C3", "C4", "C5", "C6"],
+        "row2" : ["C7", "C8", "C9", "C10", "C11", "C12"]
+},
+    "large spots": {
+        "row1" : ["L1", "L2", "L3", "L4", "L5", "L6"],
+        "row2" : ["L7", "L8", "L9", "L10", "L11", "L12"]
+}
+},
+"Level-2": {
+     "Motorcycle spots" : {
+        "row1": ["M1", "M2", "M3", "M4", "M5", "M6"],
+        "row2": ["M7", "M8", "M9", "M10", "M11", "M12"]
+},
+    "compact spots" : {
+        "row1" : ["C1", "C2", "C3", "C4", "C5", "C6"],
+        "row2" : ["C7", "C8", "C9", "C10", "C11", "C12"]
+},
+    "large spots": {
+        "row1" : ["L1", "L2", "L3", "L4", "L5", "L6"],
+        "row2" : ["L7", "L8", "L9", "L10", "L11", "L12"]
+}
+},
+"Level-3": {
+     "Motorcycle spots" : {
+        "row1":["M1", "M2", "M3", "M4", "M5", "M6"],
+        "row2":["M7", "M8", "M9", "M10", "M11", "M12"]
+},
+    "compact spots" : {
+        "row1" : ["C1", "C2", "C3", "C4", "C5", "C6"],
+        "row2" : ["C7", "C8", "C9", "C10", "C11", "C12"]
+},
+    "large spots": {
+        "row1" : ["L1", "L2", "L3", "L4", "L5", "L6"],
+        "row2" : ["L7", "L8", "L9", "L10", "L11", "L12"]
+}
+}
+}
