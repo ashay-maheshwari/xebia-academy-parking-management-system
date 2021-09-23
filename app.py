9bd27b01d0d9ec1d7e80a760ca70416c325638c9
ashay-maheshwari <aaryaashay1848@gmail.com>
2021-09-23 22:17:35 +0530

    add modularity

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from config import *
 from dbinit import *
 from parkingfunctions import *
 
-print(add(5,6))
 
 app = Flask(__name__)
 
@@ -26,40 +25,8 @@ def hello():
 
 @app.route("/park-bike")
 def park_motorcycle():
-    levels = list(fetched_parking_status.keys())
-    parking_found = 0
-
-    # Remove _id key from the list
-    levels.remove("_id")
-
-    for level in levels:
-
-        # Look for a spot in Motorcycle section first
-        motorcycle_spots = list(parking_slots[level]["Motorcycle spots"].keys())
-        for row in motorcycle_spots:
-            if len(parking_slots[level]["Motorcycle spots"][row]) == 0:
-                print("No parking found")
-            else:
-                parking_found = 1
-                return "Parking found in motorcycle spot"
-
-        # Look for a parking in compact spot
-        compact_spots = list(parking_slots[level]["compact spots"].keys())
-        for row in compact_spots:
-            if len(parking_slots[level]["compact spots"][row]) == 0:
-                print("No parking found")
-            else:
-                parking_found = 1
-                return "Parking found in compact spot"
-
-        # Look for a parking in large spot
-        large_spots = list(parking_slots[level]["large spots"].keys())
-        for row in large_spots:
-            if parking_slots[level]["large spots"][row] == []:
-                print("No parking found")
-            else:
-                parking_found = 1
-                return "Parking found in a large spot"
+    check_parking = bike_parking("TS07HK2216","15", fetched_parking_status)
+    return check_parking
 
 
 if __name__ == '__main__':
--- parkingfunctions.py
+++ parkingfunctions.py
@@ -1,2 +1,70 @@
-def add(x,y):
-    return x+y
+from datetime import date
+today = date.today()
+# dd/mm/YY
+date_today = today.strftime("%d/%m/%Y")
+
+def bike_parking(bike_no, rate, latest_parking_slots):
+    """
+    :param bike_no: Registration number of the bike
+    :param rate: rate of parking in INR
+    :param latest_parking_slots: fetch the latest parking spots available in the database
+    :return: JSON response with bike_no, date, parking time, parking slot address
+    """
+
+    # Dictionary to be provided with response
+    parking_response_dict = {
+        "Vehicle Registration No" : bike_no,
+        "Parking Rate" : rate,
+        "Date" : date_today
+    }
+
+    levels = list(latest_parking_slots.keys())
+    parking_found = 0
+
+    # Remove _id key from the list
+    levels.remove("_id")
+
+    for level in levels:
+
+        # Look for a spot in Motorcycle section first
+        motorcycle_spots = list(latest_parking_slots[level]["Motorcycle spots"].keys())
+        for row in motorcycle_spots:
+            if len(latest_parking_slots[level]["Motorcycle spots"][row]) == 0:
+                print("No parking found")
+            else:
+                parking_found = 1
+                parking_response_dict["Level"] = level
+                parking_response_dict["Slot Category"] = "Motorcycle Slot"
+                parking_response_dict["Row"] = row
+                parking_response_dict["Spot"] = ""
+
+                return parking_response_dict
+
+        # Look for a parking in compact spot
+        compact_spots = list(latest_parking_slots[level]["compact spots"].keys())
+        for row in compact_spots:
+            if len(latest_parking_slots[level]["compact spots"][row]) == 0:
+                print("No parking found")
+            else:
+                parking_found = 1
+                parking_response_dict["Level"] = level
+                parking_response_dict["Slot Category"] = "Compact Slot"
+                parking_response_dict["Row"] = row
+                parking_response_dict["Spot"] = ""
+
+                return parking_response_dict
+
+        # Look for a parking in large spot
+        large_spots = list(latest_parking_slots[level]["large spots"].keys())
+        for row in large_spots:
+            if len(latest_parking_slots[level]["large spots"][row]) == 0:
+                print("No parking found")
+            else:
+                parking_found = 1
+                parking_response_dict["Level"] = level
+                parking_response_dict["Slot Category"] = "Large Slot"
+                parking_response_dict["Row"] = row
+                parking_response_dict["Spot"] = ""
+
+                return parking_response_dict
+
